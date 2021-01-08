bb2f962efc00a95fa4c29db168651cee09cf002d
yifzheng <70163313+yifzheng@users.noreply.github.com>
2021-01-07 20:54:50 -0500

    Update color.js
    added comments on what problems functions are solving

--- color.js
+++ color.js
@@ -1,6 +1,9 @@
+// when clicking on the options from drop down button, changes the currentColor based on select color
 function selectColor(color) {
     currentColor = color;
 }
+
+// fills all the cells with currentColor disregarding if the cell already has a non-initial background color
 function fillAll() {
     let cells = document.getElementsByClassName("grid-cell");
     let arr = Array.from(cells);
@@ -12,6 +15,8 @@ function fillAll() {
         cell.style.backgroundColor = currentColor;
     });
 }
+
+// similar to fillAll() in which it fills all cells that don't have a colored background
 function fillRest(){
     let cells = document.getElementsByClassName("grid-cell");
     let arr = Array.from(cells);
@@ -22,26 +27,36 @@ function fillRest(){
         }
     });
 }
+
+// function that changes the currentColor as well as the color of the button to let users know what the current color is 
 function changeAzure(){
     currentColor = "azure";
     let element = document.getElementById("btn");
     element.style.backgroundColor = 'azure';
 }
+
+// function that changes the currentColor as well as the color of the button to let users know what the current color is
 function changeOrange(){
     currentColor = "orange";
     let element = document.getElementById("btn");
     element.style.backgroundColor = 'orange';
 }
+
+// function that changes the currentColor as well as the color of the button to let users know what the current color is
 function changeRed(){
     currentColor = "red";
     let element = document.getElementById("btn");
     element.style.backgroundColor = 'red';
 }
+
+// function that changes the currentColor as well as the color of the button to let users know what the current color is
 function changeBlue(){
     currentColor = "blue"
     let element = document.getElementById("btn");
     element.style.backgroundColor = 'blue';
 }
+
+// function that changes the currentColor as well as the color of the button to let users know what the current color is
 function changeGreen(){
     currentColor = "green"
     let element = document.getElementById("btn");
