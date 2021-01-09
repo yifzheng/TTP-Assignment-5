07da58ff3c86020c96bbfa2dd0ece97be44f2c61
yifzheng <70163313+yifzheng@users.noreply.github.com>
2021-01-09 15:02:50 -0500

    added comments to code

--- clear.js
+++ clear.js
@@ -1,7 +1,10 @@
 // function to clear all background color
 function clearAll(){
+    // nodeList of every cell in the grid
     let cells = document.getElementsByClassName("grid-cell");
+    // makes and array from the nodeList
     let arr = Array.from(cells);
+    // for every cell in the list we make the background color null, remove all class names available, adds 2 new class names grid-cell and uncolored
     arr.forEach(cell =>
     {
         cell.style.backgroundColor = null;
